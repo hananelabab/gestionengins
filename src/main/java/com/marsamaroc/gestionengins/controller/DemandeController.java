e242b820d43ef496e1eb088f27f95465e0b1589f
NouamaneDahhak <61422720+NouamaneDahhak@users.noreply.github.com>
2022-03-10 12:48:32 +0100

    edit DeleteMapping

--- src/main/java/com/marsamaroc/gestionengins/controller/DemandeController.java
+++ src/main/java/com/marsamaroc/gestionengins/controller/DemandeController.java
@@ -111,7 +111,7 @@ public class DemandeController {
     }
 
 
-    @DeleteMapping(value="/supenginaffect")
+    @PostMapping(value="/supenginaffect")
     EnginAffecte deletEnginAffect(@RequestBody EnginAffecte enginAffecte){
         enginAffecteService.delete(enginAffecte);
         return enginAffecte;
